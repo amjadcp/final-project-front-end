c54beb8ffb27c22ec43b81190f7734af89c32ccd
dilshad <dilshadbaghdad@gmail.com>
2023-03-15 13:22:32 +0300

    'modified'

--- src/routes/trading-view/+page.svelte
+++ src/routes/trading-view/+page.svelte
@@ -14,35 +14,18 @@
 	<HeroBanner
 		slot="hero"
 		image={heroImage}
-		title="Trading data"
-		subtitle="Explore and download trading data from multiple blockchains and decentralised exchanges."
+		title="Department Of Vocational Studies"
+		subtitle="Explore the courses BVOC SOFTWARE DEVELOPMENT and BVOC AUTOMOBILE ELECTRICAL AND ELECTRONICS "
 	/>
 
-	<ContentCardsSection title="Explore data">
-		<ContentCard icon="blockchain" title="Blockchains" ctaLabel="Explore blockchains" href="/trading-view/blockchains">
-			<p>Trading Strategy provides powerful market data sets for on-chain trading on several blockchains.</p>
-			{#if data}
-				<p>Currently indexing data from <strong>4 blockchains</strong></p>
-			{/if}
-		</ContentCard>
+	<ContentCardsSection title="Explore">
 
-		<ContentCard icon="exchange" title="DEXes" ctaLabel="Browse DEXes" href="/trading-view/exchanges">
+		<ContentCard icon="exchange" title="BVOC SOFTWARE DEVELOPMENT" ctaLabel="Browse BVocSD" href="/trading-view/exchanges">
 			<p>
-				Trading Strategy provides data sets for decentralised exchanges. All market data is sourced from on-chain
-				trades, across multiple DEXs.
+				Bachelor of Vocational or BVoc Software Development deals with subjects such as programming, software, coding, economic, social analysis, basics of computer, computer networking etc. This course is mainly pursued by candidates who are interested in computer software and want to pursue their career in the IT industry.
 			</p>
 			{#if data}
-				<p>Currently indexing data from <strong>{formatAmount(data.exchanges)} DEXes</strong></p>
-			{/if}
-		</ContentCard>
-
-		<ContentCard icon="pair" title="Trading pairs" ctaLabel="Browse trading pairs" href="/trading-view/trading-pairs">
-			<p>
-				Trading pairs have OHLCV candle data available between 1-minute to 30-day time frames. View historical and
-				current datasets here.
-			</p>
-			{#if data}
-				<p>Currently indexing data from <strong>{formatAmount(data.pairs)} trading pairs</strong></p>
+				
 			{/if}
 		</ContentCard>
 
@@ -56,38 +39,12 @@
 			{/if}
 		</ContentCard>
 
-		<ContentCard icon="dictionary" title="DeFi and Trading Dictionary" ctaLabel="View dictionary" href="/glossary">
-			<p>Browse and learn DeFi, trading and technical analysis terminology.</p>
-		</ContentCard>
-	</ContentCardsSection>
-
-	<ContentCardsSection title="Programmatic access">
-		<ContentCard icon="backtesting" title="Backtesting" ctaLabel="Download datasets" href="/trading-view/backtesting">
-			<p>
-				Download historical OHLCV data for backtesting your trading algorithms. Liquidity information is available for
-				calculating past slippage. Datasets are served in Parquet file format.
-			</p>
-			{#if data}
-				<p>Currently providing <strong>{formatSizeGigabytes(data.database_size)} GB worth of data</strong></p>
-			{/if}
-		</ContentCard>
-
-		<ContentCard
-			icon="24h"
-			title="Realtime API"
-			ctaLabel="Read API specification"
-			href="https://tradingstrategy.ai/api/explorer/"
-		>
-			<p>Connect your trading algorithms for real-time market feeds for live trading.</p>
-			<p>Real-time API is available in OpenAPI v3 format, no API keys or sign ups needed.</p>
-		</ContentCard>
-
 		<ContentCard
 			icon="book"
 			title="Documentation"
 			ctaLabel="Read documentation"
 			href="https://tradingstrategy.ai/docs"
-			description="Trading Strategy provides Python libraries for strategy development and execution for decentralised exchanges. Read API documentation and tutorials to learn how to create your own strategies."
+			description="You can read our documentation here"
 		/>
 
 		<ContentCard
@@ -95,7 +52,7 @@
 			title="Notebooks"
 			ctaLabel="Go to notebooks"
 			href="https://tradingstrategy.ai/docs/programming/strategy-examples/index.html"
-			description="Use popular Jupyter Notebook, Pandas and other data science libraries to model and backtest your strategies. View example notebooks to see how to use DeFi data in your notebooks."
+			description="You can download all notes here"
 		/>
 	</ContentCardsSection>
 </ContentCardsTemplate>
