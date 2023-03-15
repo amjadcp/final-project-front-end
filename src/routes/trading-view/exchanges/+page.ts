581ca696016d6ea323f142f95086b6637741570d
shana yasmin <shanayasmin2003@gmail.com>
2023-03-16 09:46:53 +1400

    files are delete

--- src/routes/trading-view/exchanges/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from '../$types';
-import { fetchPublicApi } from '$lib/helpers/public-api';
-
-// https://tradingstrategy.ai/api/explorer/#/Exchange/web_exchanges
-export const load = (async ({ fetch }) => {
-	return fetchPublicApi(fetch, 'exchanges');
-}) satisfies PageLoad;
